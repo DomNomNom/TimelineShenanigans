1c3c3e25c8feff66322299bd5c592e4455c91fbc
Dominik Schmid <DominikSchmid93@gmail.com>
2015-06-09 17:43:36 +1200

    Added buildPointers() to Graph

--- createDataStructure.cs
+++ createDataStructure.cs
@@ -14,6 +14,42 @@ data (JSON)
     Link has keys for 2 Bodys: source and target
     A moment does not hold any information.
 
+    graph = {
+        descriptions: {
+            "John": {
+                "name": "John",
+                "colour": "#0715CD",
+                "image": "john.png",
+                "groups": [
+                    "Kids"
+                ]
+            },
+        }
+
+        moments: {
+            1901: {}
+            1902: {}
+        }
+
+        bodies: {
+            "888881": {
+                key_moment: 1901
+                key_description: John
+            },
+            "888882": {
+                key_moment: 1902
+                key_description: John
+            }
+        }
+
+        links: [
+            {
+                key_prev: 888881
+                key_next: 888882
+            }
+        ]
+    }
+
 Generated relations (object references):
     moment has a list of characters
     Bodys have a list of links:     links where we are the source
@@ -49,7 +85,7 @@ class Graph
             # These ones are special cases where the name index
             # is the same but something else is different.
             # By default it's the name of the character.
-            # Every used combination should be stored in here
+            # Every used combination should be stored in here before use
             "4,2,2,0": "Dave"
             "4,2,3,0": "Dave (Scratch)"
             "6,3,4,0": "Jade"
@@ -132,13 +168,15 @@ class Graph
             for i in [0 .. panel.length-1]
                 for oldlink in panel[i][4]
                     link = {
-                        key_source: panel_index2bodyID[panelID    + ',' + i         ]
-                        key_target: panel_index2bodyID[oldlink[0] + ',' + oldlink[1]]
+                        key_prev: panel_index2bodyID[panelID    + ',' + i         ]
+                        key_next: panel_index2bodyID[oldlink[0] + ',' + oldlink[1]]
                     }
                     @links.push(link)
 
         # check that nothing is wrong so far
         @validate()
+
+        @buildPointers()
 
 
     # appends a new body to the list
@@ -149,19 +187,69 @@ class Graph
     validate: () ->
         # all keys in links must exist
         for link in @links
-            assert link.key_source of @bodies
-            assert link.key_target of @bodies
+            assert link.key_prev of @bodies
+            assert link.key_next of @bodies
 
         # all keys in bodies must exist
         for key, body of @bodies
             assert body.key_moment of @moments
             assert body.key_description of @descriptions
 
-        return true
+
+    # this builds the direct links that are not represented in JSON
+    buildPointers: () ->
+
+        for id, description of @descriptions
+            description.bodies = []
+
+        for id, moment of @moments
+            moment.bodies = []
+
+        # create direct pointers for links
+        for id, body of @bodies
+            body.prev = []
+            body.next = []
+            body.links_prev = []
+            body.links_next = []
+
+            body.description = @descriptions[body.key_description]
+            body.description.bodies.push(body)
+
+            body.moment = @moments[body.key_moment]
+            body.moment.bodies.push(body)
+
+        for link in @links
+            prev = @bodies[link.key_prev]
+            next = @bodies[link.key_next]
+            link.prev = prev
+            link.next = next
+            next.prev.push(prev)
+            prev.next.push(next)
+            next.links_prev.push(link)
+            prev.links_next.push(link)
+
+    # delete things so that we get back to what we had before buildPointers()
+    deletePointers: () ->
+        for id, description of @descriptions
+            delete description.bodies
+
+        for id, body of @bodies
+            delete body.prev
+            delete body.next
+            delete body.links_prev
+            delete body.links_next
+            delete body.description
+            delete body.moment
+
+        for link in @links
+            delete link.prev
+            delete link.next
 
 
 
 graph = new Graph(timelines)
+graph.deletePointers()
+
 # log JSON.stringify(graph, null, 4)
 log 'stringify 1'
 # txt = JSON.stringify(graph, null, 4)
